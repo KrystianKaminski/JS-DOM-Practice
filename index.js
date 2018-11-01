194e82ef19071994cdc69e6a2638728c54d09b7b
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-11-01 19:28:25 +0100

    Recreate addItem function

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 // 1. Adding item to list
 
-const inputValue = document.querySelector('.input__text');
-const inputBtn = document.querySelector('.input__button');
-const list = document.querySelector('.list');
-const deleteBtn = document.querySelectorAll('.list__item--delete');
+const inputValue = document.querySelector(".input__text");
+const inputBtn = document.querySelector(".input__button");
+const list = document.querySelector(".list");
+const listItem = document.querySelector(".list__item");
+const deleteBtn = document.querySelectorAll(".list__item--delete");
 
 const addItem = () => {
-    const text = document.createTextNode(inputValue.value);
-    const li = document.createElement('li');
-    const span = document.createElement('span');
-    li.setAttribute('class', 'list__item');
-    span.setAttribute('class', 'list__item--delete');
-    span.textContent = 'X';
-    li.appendChild(text);
-    li.appendChild(span);
-    list.appendChild(li);
-    inputValue.value = "";
-}
+  const listHTML = `<li class="list__item">${
+    inputValue.value
+  }<span class="list__item--delete">X</span></li>`;
+  list.insertAdjacentHTML("beforeend", listHTML);
+};
 
+const clearInput = () => {
+  inputValue.value = "";
+};
 
-inputBtn.addEventListener('click', addItem);
+inputBtn.addEventListener("click", () => {
+  event.preventDefault();
+  addItem();
+  clearInput();
+});
